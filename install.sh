c6c9848aaaa6dc095ccfe2264ad0209b0a577991
Igor Vashyst <ivashyst@gmail.com>
2023-07-07 19:20:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,23 +30,7 @@ git config --global core.excludesfile "~/.gitignore_global"
 git config --global core.autocrlf input
 git config --global core.editor vim
 git config --global alias.co checkout
+git config --global alias.slog "log --pretty=oneline --abbrev-commit"
 
-
-# ----------------------------------------------------------------------------
-# Sublime
-SUBLIME_PACKS_DIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-cd "$SUBLIME_PACKS_DIR"
-
-# Soda Theme
-SODA_DIR="Theme - Soda"
-if [ ! -d "$SUBLIME_PACKS_DIR/$SODA_DIR" ]; then
-    git clone https://github.com/buymeasoda/soda-theme/ "$SODA_DIR"
-fi
-
-# Base16 Color Scheme
-BASE16_DIR="Base16 Color Schemes"
-if [ ! -d "$SUBLIME_PACKS_DIR/$BASE16_DIR" ]; then
-    git clone https://github.com/chriskempson/base16-textmate/ "$BASE16_DIR"
-fi
 
 echo "done."
