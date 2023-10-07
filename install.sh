03819e3f03f204c8f6079bed0788bd4e5c4ec276
Igor Vashyst <ivashyst@gmail.com>
2023-10-07 18:52:03 +0200

    Add git default branch to install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ git config --global color.ui auto
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
+git config --global init.defaultBranch main
 git config --global core.excludesfile "~/.gitignore_global"
 git config --global core.autocrlf input
 git config --global core.editor vim
